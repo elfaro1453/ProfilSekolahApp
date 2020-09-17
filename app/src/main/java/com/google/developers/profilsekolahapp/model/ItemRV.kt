370e117255775dd0926f86f4d111c4879b012aa4
elfaro1453 <54815985+elfaro1453@users.noreply.github.com>
2020-09-17 08:40:21 +0700

    update 17/09/2020

---
+++ app/src/main/java/com/google/developers/profilsekolahapp/model/ItemRV.kt
@@ -0,0 +1,11 @@
+package com.google.developers.profilsekolahapp.model
+
+/**
+ * Created by Imam Fahrur Rofi on 16/09/2020.
+ */
+data class ItemRV(
+    var urlGambar: String = "",
+    var title: String = "",
+    var description: String = "",
+    var tipe: String = ""
+)
